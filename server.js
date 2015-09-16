87915a17ba127a3f2a9817ae637d3bc4b34a05d6
Catherine <ducharme.catherine@gmail.com>
2015-09-16 23:38:41 +0000

    Created a very simple web server.

---
+++ server.js
@@ -0,0 +1,12 @@
+
+var http = require('http');
+
+const PORT=8080;
+
+function handleRequest(request, response){
+    response.end("Hello World");
+}
+
+var server = http.createServer(handleRequest);
+
+server.listen(process.env.PORT, process.env.IP);
