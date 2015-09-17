7912e321db747fb5e8eae17e519400bfe52d5957
Catherine <ducharme.catherine@gmail.com>
2015-09-17 15:07:44 +0000

    Created an unusable web server.

---
+++ unusable-server.js
@@ -0,0 +1,16 @@
+
+var http = require('http');
+
+const PORT=8080;
+
+function handleRequest(request, response){
+    var start = new Date().getTime();
+    while (new Date().getTime() - start < 10000);
+// INSERT YOUR res.writeHead / res.end lines after this line
+    response.writeHead(200);
+    response.end('Hello World');
+};
+
+var server = http.createServer(handleRequest);
+
+server.listen(process.env.PORT, process.env.IP);
