9bbbeff92c3ddf7f3f3cd7ea273011cfb384e6d3
Catherine <ducharme.catherine@gmail.com>
2015-09-18 16:48:06 +0000

    Removed a bunch of test code I had forgotten in from cps.js.

--- cps.js
+++ cps.js
@@ -32,32 +32,3 @@ function getFirstAndLast(word, callback) {
 
 getFirstAndLast("hello", function(newStr) { console.log(newStr); }); 
 
-
-
-function getFirstChar(str, cb) {
-    return str[0];
-}
-function getLastChar(str, cb) {
-    return str[str.length - 1];
-}
-function getFirstAndLast(str) {
-    var first = getFirstChar(str);
-    var last = getLastChar(str);
-    
-    return first + last;
-}
-var result = getFirstAndLast("hello");
-console.log(result);
-
-
-
-request('google', function(err, resp) {
-    prompt(['city'], function(err2, res2) {
-        console.log();
-    })
-})
-
-
-var resp = request('google');
-var res2 = prompt(['city']);
-console.log();
